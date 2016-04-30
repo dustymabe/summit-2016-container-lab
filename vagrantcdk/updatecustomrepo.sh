4b4e3835752c0e65f1d515e328b207a83f49599f
Dusty Mabe <dusty@dustymabe.com>
2016-04-29 22:51:44 -0400

    blah

--- vagrantcdk/updatecustomrepo.sh
+++ vagrantcdk/updatecustomrepo.sh
@@ -1,3 +1,6 @@
+
+
+repofile=$(cat <<EOF
 #   [rhel-x86_64-server-7]
 #   baseurl = http://content.example.com/packages/rhel-x86_64-server-7/
 #   sslverify = 0
@@ -18,3 +21,18 @@
 #   name = Optional
 #   enabled = 1
 #   gpgcheck = 0
+EOF
+)
+
+files=(
+./lab1/custom.repo
+./lab1/registry/custom.repo
+./lab2/bigapp/custom.repo
+./lab3/mariadb/custom.repo
+./lab3/wordpress/custom.repo
+)
+
+for file in ${files[@]}; do
+    echo "Writing to $file"
+    echo "$repofile" > $file
+done
